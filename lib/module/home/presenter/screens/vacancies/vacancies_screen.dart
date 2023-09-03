9750abc3aca387870a80104f40d34f22f43cbce4
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-09-03 14:14:55 -0300

    att

--- lib/module/home/presenter/screens/vacancies/vacancies_screen.dart
+++ lib/module/home/presenter/screens/vacancies/vacancies_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hackathon_austria/core/extensions/build_context_utils.dart';
 import 'package:hackathon_austria/core/styles/text_styles.dart';
 import 'package:hackathon_austria/module/home/presenter/widgets/bottom_navigator.dart';
 import 'package:hackathon_austria/module/home/presenter/widgets/graphic_widget.dart';
+import 'package:hackathon_austria/module/home/presenter/widgets/item_vacancies.dart';
+
+import '../../../../../core/styles/colors.dart';
 
 class VacanciesScreen extends StatelessWidget {
   const VacanciesScreen({super.key});
@@ -31,7 +35,39 @@ class VacanciesScreen extends StatelessWidget {
                 const SizedBox(
                   height: 20,
                 ),
-                const GraphicWidget()
+                const GraphicWidget(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "OPEN POSITIONS",
+                      style: GoogleFonts.inter(),
+                    ),
+                    const Icon(
+                      Icons.filter_list_sharp,
+                      size: 24,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 36,
+                  width: context.mediaWidth * 0.9,
+                  decoration: BoxDecoration(color: AppColors.secondaryGray),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const ItemVacancies(
+                  title: "Microelectronics Engineer",
+                  subtitle:
+                      "As a Semiconductor Process Engineer, you will play a crucial role in our semiconductor fabrication process.",
+                ),
               ],
             ),
           ),
---
+++ lib/module/home/presenter/widgets/item_vacancies.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hackathon_austria/core/extensions/build_context_utils.dart';
+
+import '../../../../core/styles/colors.dart';
+
+class ItemVacancies extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  const ItemVacancies(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: context.mediaWidth * 0.9,
+      decoration: const BoxDecoration(color: Colors.white70),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: GoogleFonts.inter(),
+                ),
+                SizedBox(
+                    width: 17,
+                    height: 19,
+                    child: Image.asset("assets/iconcommunity1.png"))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Text("􀼼"),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  "Vienna",
+                  style: GoogleFonts.inter(),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: context.mediaWidth * 0.6,
+                  child: Text(
+                    subtitle,
+                    style: GoogleFonts.inter(),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_sharp,
+                  size: 24,
+                  color: AppColors.primaryBlack,
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "80% Match with Your Profile",
+                style: GoogleFonts.inter(),
+              ))
+        ],
+      ),
+    );
+  }
+}
